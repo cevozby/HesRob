723faa88c33aea2d746b25abab30150225441bd1
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2022-05-08 12:47:06 +0300

    UI Elemenets and Player Management generated

--- Assets/Scripts/PlayerManagement.cs
+++ Assets/Scripts/PlayerManagement.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerManagement : MonoBehaviour
 {
-    public static int playerHealth = 100;
+    Robot player = new Robot(100, 10, 15);
+
+    public Slider healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar.minValue = 0;
+        healthBar.maxValue = player.Health;
+        healthBar.value = player.Health;
+        Debug.Log(player.Health);
+        Debug.Log(player.DamageGenerate());
     }
 
     // Update is called once per frame
