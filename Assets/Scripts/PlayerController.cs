fe1ff0c3b2c9c11b0fd8cc851853d265f63cebe3
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2022-05-25 11:44:20 +0300

    Player animation generated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,22 +34,29 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             playerRB.velocity = Vector2.right * speed;
+            playerSR.flipX = false;
+            playerAnim.SetFloat("Speed", speed);
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             playerRB.velocity = Vector2.left * speed;
+            playerSR.flipX = true;
+            playerAnim.SetFloat("Speed", speed);
         }
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+       /* else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             playerRB.velocity = Vector2.up * speed;
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             playerRB.velocity = Vector2.down * speed;
-        }
+        }*/
         else
         {
             playerRB.velocity = Vector2.zero;
+            playerAnim.SetFloat("Speed", 0);
+            playerSR.flipX = false;
+            
         }
     }
 
