099a0bb281382a10f85302781b2f2d71d1745c5e
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2022-06-05 01:25:02 +0300

    New elevator script generated

---
+++ Assets/Scripts/Elevator/YellowElevator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowElevator : MonoBehaviour
+{
+    Vector3 startPos;
+    [SerializeField] Vector3 endPos;
+    public Transform player;
+
+    bool fControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElevatorPos();
+    }
+
+    void ElevatorPos()
+    {
+        if (fControl && Input.GetKeyDown(KeyCode.F))
+        {
+            if (transform.position == startPos)
+            {
+                transform.position = endPos;
+                player.position = new Vector3(endPos.x, endPos.y + 0.8f, player.position.z);
+                fControl = false;
+            }
+            else if (transform.position == endPos)
+            {
+                transform.position = startPos;
+                player.position = new Vector3(startPos.x, startPos.y + 0.8f, player.position.z);
+                fControl = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && KabloPaneli.sariAsansor)
+        {
+            fControl = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            fControl = false;
+        }
+    }
+}
