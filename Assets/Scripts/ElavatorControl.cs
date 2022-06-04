0196278769d17fc1fb86908dc84ff752132d31f7
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2022-06-04 13:24:31 +0300

    Elevator script generated

--- Assets/Scripts/ElavatorControl.cs
+++ Assets/Scripts/ElavatorControl.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class ElavatorControl : MonoBehaviour
 {
+    Vector3 startPos;
+    [SerializeField] Vector3 endPos;
+    public Transform player;
+
+    bool fControl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ElevatorPos();
     }
+
+    void ElevatorPos()
+    {
+        if(fControl && Input.GetKeyDown(KeyCode.F))
+        {
+            if(transform.position == startPos)
+            {
+                transform.position = endPos;
+                player.position = new Vector3(endPos.x, endPos.y + 0.8f, player.position.z);
+                fControl = false;
+            }
+            else if(transform.position == endPos)
+            {
+                transform.position = startPos;
+                player.position = new Vector3(startPos.x, startPos.y + 0.8f, player.position.z);
+                fControl = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            fControl = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            fControl = false;
+        }
+    }
+
 }
